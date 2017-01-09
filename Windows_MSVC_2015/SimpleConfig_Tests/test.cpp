6a9bd2d9dc2cc9d90dcb455e3729d79a24b715c6
Ricky Neil <rick.neil@thalesgroup.com.au>
2017-01-09 11:08:17 +1100

    Added more unit test cases for the default parser.

--- Windows_MSVC_2015/SimpleConfig_Tests/test.cpp
+++ Windows_MSVC_2015/SimpleConfig_Tests/test.cpp
@@ -26,5 +26,63 @@ namespace SimpleConfig_Tests
 			Assert::AreEqual( TSTRING( TEXT( "Duplicate Configuration Key: TestKey" ) ), testParser.CheckMessage() );
 		}
 
+		TEST_METHOD( DefaultParser_Parse )
+		{
+			DefaultParser testParser( TEXT( "TestSection" ) );
+			
+			/* check that the duplicate message is not sent on successful parse. */
+			testParser.Parse( TEXT( "TestKey" ), TEXT( "1" ) );
+			Assert::AreEqual( TSTRING( TEXT( "" ) ), testParser.CheckMessage() );
+
+			/* check that the item was actually inserted into the mapping. */
+			Assert::AreEqual( TSTRING( TEXT( "1" ) ), testParser.getString( TEXT( "TestKey" ), TEXT( "NULL" ) ) );
+
+			/* check that the duplicate message is send on failed parse. */
+			testParser.Parse( TEXT( "TestKey" ), TEXT( "1" ) );
+			Assert::AreEqual( TSTRING( TEXT( "Duplicate Configuration Key: TestKey" ) ), testParser.CheckMessage() );
+
+			/* test entering a few values for the mapping. */
+			testParser.Parse( TEXT( "TestKey2" ), TEXT( "2" ) );
+			testParser.Parse( TEXT( "TestKey3" ), TEXT( "3" ) );
+			testParser.Parse( TEXT( "TestKey4" ), TEXT( "4" ) );
+			testParser.Parse( TEXT( "TestKey5" ), TEXT( "5" ) );
+			testParser.Parse( TEXT( "TestKey6" ), TEXT( "6" ) );
+			testParser.Parse( TEXT( "TestKey7" ), TEXT( "7" ) );
+
+			/* see if they are all mapped correctly */
+			Assert::AreEqual( TSTRING( TEXT( "1" ) ), testParser.getString( TEXT( "TestKey" ), TEXT( "" ) ) );
+			Assert::AreEqual( TSTRING( TEXT( "2" ) ), testParser.getString( TEXT( "TestKey2" ), TEXT( "" ) ) );
+			Assert::AreEqual( TSTRING( TEXT( "3" ) ), testParser.getString( TEXT( "TestKey3" ), TEXT( "" ) ) );
+			Assert::AreEqual( TSTRING( TEXT( "4" ) ), testParser.getString( TEXT( "TestKey4" ), TEXT( "" ) ) );
+			Assert::AreEqual( TSTRING( TEXT( "5" ) ), testParser.getString( TEXT( "TestKey5" ), TEXT( "" ) ) );
+			Assert::AreEqual( TSTRING( TEXT( "6" ) ), testParser.getString( TEXT( "TestKey6" ), TEXT( "" ) ) );
+			Assert::AreEqual( TSTRING( TEXT( "7" ) ), testParser.getString( TEXT( "TestKey7" ), TEXT( "" ) ) );
+		}
+
+		TEST_METHOD( DefaultParser_getString )
+		{
+			DefaultParser testParser( TEXT( "TestSection" ) );
+		}
+
+		TEST_METHOD( DefaultParser_getInt16 )
+		{
+			DefaultParser testParser( TEXT( "TestSection" ) );
+		}
+
+		TEST_METHOD( DefaultParser_getInt32 )
+		{
+			DefaultParser testParser( TEXT( "TestSection" ) );
+		}
+
+		TEST_METHOD( DefaultParser_getInt64 )
+		{
+			DefaultParser testParser( TEXT( "TestSection" ) );
+		}
+
+		TEST_METHOD( DefaultParser_getDouble )
+		{
+			DefaultParser testParser( TEXT( "TestSection" ) );
+		}
+
 	};
 }
